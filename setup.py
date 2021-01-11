c94248475720a279827e6fe59fffcb7baa3d0973
bluecereal <singedsalam@gmail.com>
2021-01-11 01:40:59 -0500

    figured out setuptools stuff

--- setup.py
+++ setup.py
@@ -6,20 +6,12 @@ with open("README.md", "r") as fh:
 	
 
 setuptools.setup(
-	name='box-eye',
+	name='boxeye',
 	version='0.1',
-	# scripts=['src'],
-	author="bot-boi & bluecereal",
-	author_email="N/A",
-	description="A highly constrain-able computer vision bot framework for Android emulators.",
-	long_description=long_description,
-	long_description_content_type="text/markdown",
 	url="https://github.com/bot-boi/box-eye",
-	packages={'box_eye': 'src'},
-	package_dir={'box_eye': 'src'},
+	packages=['boxeye'],
 	classifiers=[
 		"Programming Language :: Python :: 3",
 		"License :: OSI Approved :: MIT License",
-		"Operating System :: OS Independent",
 	],
 )

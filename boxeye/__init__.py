b424718a512b2ece1f1f8e64697a3e5ba5cc3e3d
bot-boi <bot-boi@gmail.com>
2021-02-28 22:15:06 -0500

    delete Region, force point values to int

--- boxeye/__init__.py
+++ boxeye/__init__.py
@@ -52,11 +52,8 @@ def _binarize(img, threshold=150):
     return img
 
 
-class Point(vectormath.Vector2):
-    def __init__(self, x, y):
-        x = int(x)  # default type for Vector2 is float
-        y = int(y)  # screencoords dont have fractions
-        super().__init__(x, y)
+def Point(x, y):
+    return vectormath.Vector2(x, y).astype(int)
 
 
 class Pattern():
@@ -239,21 +236,17 @@ class ImagePattern(Pattern):
     def locate(self, img=None):
         if img is None:
             img = capture()
-        # if self.mode != img.mode:
-        #     img = img.convert(self.mode)
         if self.grayscale:
             img = _grayscale(img)
 
-        # UGLY
         # gotta convert to pyag's region (x0, y0, x1, y1)
         p1, p2 = self.region
-        region = (int(p1.x), int(p1.y),
-                  int(p2.x), int(p2.y))
+        region = (p1.x, p1.y, p2.x, p2.y)
+        # REVIEW \/ ^ do i still need to convert?
         res = pyag.locate(self.target, img,
                           confidence=self.confidence,
                           region=region)
-        if "ads" not in self.name:
-            logger.debug("located {} at {}".format(self.name, res))
+        logger.debug("located {} at {}".format(self.name, res))
         return [res] if res is not None else []
 
 
@@ -280,13 +273,10 @@ class PatternList(Pattern):
             p_name = pattern.name
             p_names = len(p_loc) * [p_name]
             names = names + p_names
-
         return (names, loc)
-
 
     def locate(self, img=None):
         return self.locate_names(img=img)[1]
-
 
     def isvisible(self, img=None):
         bools = [p.isvisible(img=img) for p in self.data]
@@ -294,174 +284,3 @@ class PatternList(Pattern):
             return False not in bools
         else:  # match any
             return True in bools
-
-
-class Region(): # REVIEW
-    """Region.
-
-    Region is for when you want to read text from
-        somewhere on the screen.
-
-    """
-    def __init__(self, p1: Point, p2: Point):
-        p1 = p1.astype(int)
-        p2 = p2.astype(int)
-        self.p1 = p1  # closer to 0,0
-        self.p2 = p2  # farther from 0,0
-
-    @property
-    def width(self):
-        return (self.p2 - self.p1).x
-
-    @property
-    def height(self):
-        return (self.p2 - self.p1).y
-
-    @property
-    def middle(self):
-        return ((self.p2 - self.p1) // 2) + self.p1
-
-    @property
-    def region(self):
-        return (self.p1, self.p2)
-
-    # @property
-    # def region(self):
-    #     return (self.p1.x, self.p1.y, self.p2.x, self.p2.y)
-
-    def tap(self):
-        logger.debug("tap @ {}".format(self.middle))
-        tap(self.middle)
-
-    def hold(self, ms=3000):
-        hold(self.middle, ms=ms)
-        logger.debug("holding @ {} for {}ms".format(self.middle, ms))
-
-    def crop(self, img: Image) -> Image:
-        return img.crop((self.p1.x, self.p1.y, self.p2.x, self.p2.y))
-
-
-    def readboxes(self, scale=10, threshold=200, invert=True,
-                  config='', whole_img=None) -> str:
-        if whole_img is None:
-            whole_img = capture()
-        crop_img = self.crop(whole_img)
-
-        # Tesseract Improving Quality
-        img = ImageOps.scale(crop_img, scale)
-        img = binarize(img, threshold=threshold)
-        if invert:
-            img = ImageOps.invert(img)
-        img = img.filter(ImageFilter.GaussianBlur(radius=2))
-        img = img.crop(img.getbbox())
-        img = ImageOps.expand(img, border=10, fill='white')  # this is imprtant
-        # feed to tesseract, remove newlines (\r \n \x0c etc)
-        boxes = pytesseract.image_to_boxes(img, lang='eng', config=config)
-        (_, crop_height) = crop_img.size
-        (width, height) = whole_img.size
-        boxes = [b for b in boxes.splitlines()]
-        nboxes = []
-        # draw = ImageDraw.Draw(whole_img)
-        for box in boxes:
-            box = box.split(" ")
-            c = box[0]  # the char that was found
-            box = [int(i) // scale for i in box[1:]]
-            (x1, y2, x2, y1, _) = box  # last value is page number
-            # offset to screen coordinates cus of cropping
-            ny1 = crop_height - y1  # convert from bot left (tesseract)
-            ny2 = crop_height - y2  # to top left origin (swap y and flip)
-            x1 += self.p1.x
-            ny1 += self.p1.y
-            x2 += self.p1.x
-            ny2 += self.p1.y
-            p1 = Point(x1, ny1)
-            p2 = Point(x2, ny2)
-            box = (c, (p1, p2))
-            # logger.debug(box)
-            nboxes.append(box)
-            # draw.rectangle([p1.x, p1.y, p2.x, p2.y], outline="red", width=3)
-
-        # whole_img.show()
-        # img.show()
-        return nboxes
-
-    def readwordboxes(self, scale=10, confidence=0.8, threshold=200,
-                      invert=True, config='--psm 11',
-                      whole_img=None) -> str:
-        if whole_img is None:
-            whole_img = capture()
-        # Tesseract Improving Quality
-        crop_img = self.crop(whole_img)
-        img = ImageOps.scale(crop_img, scale)
-        img = binarize(img, threshold=threshold)
-        if invert:
-            img = ImageOps.invert(img)
-        img = img.filter(ImageFilter.GaussianBlur(radius=2))
-        # img = img.crop(img.getbbox())
-        img = ImageOps.expand(img, border=10, fill='white')  # this is imprtant
-
-        # feed to tesseract, remove newlines (\r \n \x0c etc)
-        (_, crop_height) = crop_img.size
-        (width, height) = whole_img.size
-        # draw = ImageDraw.Draw(whole_img)
-        data = pytesseract.image_to_data(img, lang='eng', config=config,
-                                         output_type=Output.DICT)
-        out = []
-        for text, conf, x1, y1, w, h in zip(data["text"], data["conf"],
-                                            data["left"], data["top"],
-                                            data["width"], data["height"]):
-            conf = float(conf)
-            text = text.replace(" ", "")
-            if conf >= confidence and len(text) > 0:
-                x2 = x1 + w
-                y2 = y1 + h
-                x1 = x1 // scale
-                y1 = y1 // scale
-                x2 = x2 // scale
-                y2 = y2 // scale
-                x1 += self.p1.x  # offset to mainscreen
-                y1 += self.p1.y
-                x2 += self.p1.x
-                y2 += self.p1.y
-                p1 = Point(x1, y1)
-                p2 = Point(x2, y2)
-                out.append((text, conf, (p1, p2)))
-                # draw.rectangle([p1.x, p1.y, p2.x, p2.y],
-                #                outline="red", width=2)
-
-        # whole_img.show()
-        # img.show()
-        return out
-
-    def readnumbers(self, scale=10, threshold=200, config="--psm 8 -c "
-                    "tessedit_char_whitelist=,./0123456789IKMO",
-                    img=None):
-        """readnumbers.
-        do "tesseract --help-extra" for config options
-        :param scale:
-        :param threshold:
-        :param config:
-
-        """
-        text = self.readtext(scale, threshold=threshold,
-                             config=config, img=img)
-        text = text.replace(" ", "")
-        text = text.replace(",", "")
-        text = text.replace("I", "1")
-        text = text.replace("O", "0")
-        text = text.replace("/", "7")  # this is pushing it...
-        # this way is better cus it can handle abbreviations like 1.5M
-        mul = 1
-        if "K" in text:
-            text = text.replace("K", "")
-            mul = 1000
-        elif "M" in text:
-            text = text.replace("M", "")
-            mul = 1000000
-
-        # text = "".join([i for i in text if i.isdigit()])
-        n = float(text) * mul
-        if text == '':
-            n = None
-        logger.debug("read number as {}".format(n))
-        return n

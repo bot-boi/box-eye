c69ff703b84a7d2469dfd20dcc2ebc29213721c3
bot-boi <bot-boi@zmail.com>
2021-06-08 22:13:44 -0400

    Added dependencies to setup.py and corrected version number.

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 import setuptools
 
 
-with open("README.md", "r") as fh:
-	long_description = fh.read()
-	
-
 setuptools.setup(
 	name='boxeye',
-	version='0.1',
+	version='0.0.3',
 	url="https://github.com/bot-boi/box-eye",
 	packages=['boxeye', 'boxeye/botutils'],
 	classifiers=[
 		"Programming Language :: Python :: 3",
 		"License :: OSI Approved :: MIT License",
 	],
+    install_requires=[
+        'numpy',
+        'opencv-python',
+        'Pillow',
+        'pure-python-adb',
+        'vectormath',
+    ],
 )
